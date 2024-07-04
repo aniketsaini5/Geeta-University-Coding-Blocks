5d832b27b6af58c627ff7fe445a6aa3fa9d218ae
Aniket Saini <154746704+aniketsaini5@users.noreply.github.com>
2024-07-04 14:59:23 +0530

    kush to add hi kara hoga

--- Assignment - 6274(Recursion)/mazepath.cpp
+++ Assignment - 6274(Recursion)/mazepath.cpp
@@ -1,3 +1,26 @@
+// Take as input N1 and N2, both numbers. N1 and N2 is the number of rows and columns on a rectangular board. Our player starts in top-left corner of the board and must reach bottom-right corner. In one move the player can move 1 step horizontally (right) or 1 step vertically (down) or 1 step diagonally (south-east).
+
+// Write a recursive function which:
+// a. Returns the count of different ways the player can travel across the board. Print the value returned.
+// b. Prints moves for all valid paths across the board.
+
+// Input Format
+// Enter the number of rows N1 and number of columns N2
+
+// Constraints
+// None
+
+// Output Format
+// Display the total number of paths and print all the possible paths in a space separated manner
+
+// Sample Input
+// 3
+// 3
+// Sample Output
+// VVHH VHVH VHHV VHD VDH HVVH HVHV HVD HHVV HDV DVH DHV DD
+// 13
+
+
 #include<bits/stdc++.h>
 using namespace std;
 	int n,m,c;
