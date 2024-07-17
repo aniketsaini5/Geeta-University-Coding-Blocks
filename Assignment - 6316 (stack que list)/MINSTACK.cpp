e9cb39830182a1af9038a41270f9e1a87dc2d7cb
Aniket Saini <154746704+aniketsaini5@users.noreply.github.com>
2024-07-17 14:04:48 +0530

    ading the quwe& sol of MIN stack

--- Assignment - 6316 (stack que list)/MINSTACK.cpp
+++ Assignment - 6316 (stack que list)/MINSTACK.cpp
@@ -1,3 +1,29 @@
+// Design a stack that supports push,pop,top,retrieving the minimum element in constant time.
+
+// Input Format
+// First line of input contains integer n denoting the size of the string array. Next line of input contains n space separated string where i-th string represent i-th operation.(i.e, push,pop,top,getMin)
+
+// Constraints
+// Functions pop, top and getMin operations will always be called on non-empty stacks.
+
+// Output Format
+// Print the answer according to input operations.
+
+// Sample Input
+// 7
+// push push push getMin pop top getMin
+// -2 0 -3
+// Sample Output
+// -3 0 -2 
+// Explanation
+// push -2
+// push 0
+// push -3
+// print -3 (getMin)
+// pop -3
+// print 0 (top)
+// print -2 (getMin)
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -66,4 +92,76 @@ int main() {
     }
     cout << endl;
     return 0;
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+or
+
+/////////////////////////////////////////////////////////////////////
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class MinStack {
+public:
+       stack<int> s,ss;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        s.push(val);
+        if(ss.empty()){
+            ss.push(val);
+        }else if(ss.top()>=val){
+            ss.push(val);
+        }
+        
+    }
+    
+    void pop() {
+        if(s.top()==ss.top()){
+            ss.pop();
+        }
+        s.pop();
+        
+    }
+    
+    int top() {
+        
+        return s.top();
+    }
+    
+    int getMin() {
+        return ss.top();
+        
+    }
+};
+
+int main(){
+	MinStack obj;
+	int n;
+	cin>>n;
+	vector<string> v(n);
+	for(int i=0;i<n;i++){
+		cin>>v[i];
+	}
+	
+	for(int i=0;i<n;i++){
+		string s=v[i];
+		// cin>>s;
+		if(s=="push"){
+			int x;
+			cin>>x;
+			obj.push(x);
+		}else if(s=="top"){
+			cout<<obj.top()<<" ";
+		}else if(s=="pop"){
+			obj.pop();
+		}else{
+			cout<<obj.getMin()<<" ";
+		}
+	}
+	return 0;
 }
